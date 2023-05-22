53a1d45f428462ed22de117012a38f2a7b3b07bf
Helio Souza <heliorr7@gmail.com>
2023-05-22 19:09:17 -0300

    criação do header

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <CadastroEmpresa/>
+  <HeaderCompany/>
+  <main class="content">
+    <CadastroCompany/>
+  </main>
 </template>
 
 <script>
-import CadastroEmpresa from './components/CadastroEmpresa.vue'
+import CadastroCompany from './components/Cadastro-Company.vue'
+import HeaderCompany from './components/Header-Company.vue'
 
 export default {
   name: 'App',
   components: {
-    CadastroEmpresa
+    HeaderCompany,
+    CadastroCompany
   }
 }
 </script>
 
 <style>
-#app {
+.content {
   display: flex;
   align-items: center;
   justify-content: center;
---
+++ front-end/src/components/Header-Company.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h3>Empresa</h3>
+            <section>
+                <p>Cadastro Empresa</p>
+                <p>Cadastro Fornecedor</p>
+            </section>
+    </header>
+</template>
+
+<style>
+    header {
+        display: flex;
+        flex-grow: 3;
+        justify-content: space-between;
+    }
+    h3 {
+        width: 70%;
+    }
+    section {
+        width: 30%;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
