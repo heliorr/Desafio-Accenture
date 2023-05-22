b1ce0f244bda743e45ccdc460daa420a230289e3
Helio Souza <heliorr7@gmail.com>
2023-05-22 18:55:10 -0300

    primeira versão cadastro company

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CadastroEmpresa/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CadastroEmpresa from './components/CadastroEmpresa.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CadastroEmpresa
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ front-end/src/components/CadastroEmpresa.vue
@@ -0,0 +1,47 @@
+<template>
+    <form>
+        <label for="company">
+            CNPJ:
+            <input type="text" name="CNPJ" id="CNPJ" key="CNPJ">
+        </label>
+        <label for="company">
+            Nome Fantasia:
+            <input type="text" name="company" id="company" key="company">
+        </label>
+        <label for="CEP">
+            CEP:
+            <input type="text" name="CEP" id="CEP" key="CEP">
+        </label>
+        <label for="district">
+            Bairro:
+            <input type="text" name="district" id="district" key="district">
+        </label>
+        <label for="street">
+            Rua:
+            <input type="text" name="street" id="street" key="street">
+        </label>
+        <label for="city">
+            Cidade:
+            <input type="text" name="city" id="city" key="city">
+        </label>
+        <label for="residence">
+            Numero:
+            <input type="text" name="residence" id="residence" key="residence">
+        </label>
+        <button>Cadastrar</button>
+    </form>
+</template>
+
+<style>
+form {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+}
+
+form label {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2%;
+}
+</style>
